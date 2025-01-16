c9dbe023c07d256d809175de021782e30e0b1a4f
Birçe Yakut <yakutbirce@gmail.com>
2025-01-16 18:00:30 +0300

    görev2 yapildi

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ function CemberinCevresi(yaricap) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(yaricap, pi) {
+  return pi * Math.pow(yaricap, 2);
 }
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
